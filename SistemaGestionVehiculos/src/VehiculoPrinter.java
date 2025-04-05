3843c7677d796c1e936698edcb1c58ac1cde2957
vicky <vicky@pop-os.localdomain>
2025-04-05 14:58:19 -0300

    Aplicar OCP creando clase Camion y adaptando VehiculoPrinter

--- SistemaGestionVehiculos/src/VehiculoPrinter.java
+++ SistemaGestionVehiculos/src/VehiculoPrinter.java
@@ -1,11 +1,7 @@
 public class VehiculoPrinter {
 
     public void printVehiculo(Vehiculo vehiculo){
-        System.out.println("Información del vehículo");
-        System.out.println("Patente: " + vehiculo.getPatente());
-        System.out.println("Marca: "+ vehiculo.getMarca());
-        System.out.println("Año: "+ vehiculo.getAnio());
-        System.out.println("Capacidad de carga (kg): "+ vehiculo.getCapacidadCargaKg() + "kg");
-        System.out.println("--------------------------------------------");
+        System.out.println(vehiculo.obtenerDescripcion());
+
     }
 }
